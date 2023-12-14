8a3348cce5d061de9cb4aba2094d1ed3191c4242
soywiz <soywiz@gmail.com>
2023-12-14 22:51:44 +0100

    Bumps to KorGE 5.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,44 @@
 import korlibs.korge.gradle.*
 
 plugins {
-	alias(libs.plugins.korge)
-	id("org.jetbrains.compose") version "1.4.0"
+    alias(libs.plugins.korge)
+    id("org.jetbrains.compose") version "1.5.11"
 }
 
 allprojects {
-	repositories {
-		mavenLocal()
-		mavenCentral()
-		google()
-		gradlePluginPortal()
-	}
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        google()
+        gradlePluginPortal()
+    }
 }
 
 korge {
-	id = "korlibs.korge.starterkit.compose"
+    id = "korlibs.korge.starterkit.compose"
     name = "Korge Compose"
     title = name
     icon = file("icon.png")
 
 // To enable all targets at once
 
-	//targetAll()
+    //targetAll()
 
 // To enable targets based on properties/environment variables
-	//targetDefault()
+    //targetDefault()
 
 // To selectively enable targets
-	
-	targetJvm()
-	targetJs()
-	targetDesktop()
-	targetIos()
-	targetAndroid()
 
-	serializationJson()
+    targetJvm()
+    targetJs()
+    targetDesktop()
+    targetIos()
+    targetAndroid()
+
+    serializationJson()
+
+    androidCompileSdk = 29
+    androidTargetSdk = 29
 }
 
 dependencies {
@@ -44,6 +47,6 @@ dependencies {
 }
 
 compose {
-	kotlinCompilerPluginArgs.add("suppressKotlinVersionCompatibilityCheck=1.8.21")
-	kotlinCompilerPlugin.set(dependencies.compiler.forKotlin("1.8.20"))
+    kotlinCompilerPluginArgs.add("suppressKotlinVersionCompatibilityCheck=1.9.20")
+    kotlinCompilerPlugin.set(dependencies.compiler.forKotlin("1.9.20"))
 }
